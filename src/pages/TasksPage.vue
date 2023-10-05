1d7d92699be4467d96716f918790a2945fcf134d
paalaman45 <kissel@sitesatscale.com>
2023-10-06 07:21:35 +0800

    finish app

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -6,48 +6,45 @@
                     <!-- Add new Task -->
                     <NewTask />
 
-                    <!-- list of uncompleted tasks -->
-                    <Tasks :tasks="uncompletedTasks" />
+                    <!-- List of uncompleted tasks -->
+                    <Tasks :tasks="uncompletedTasks"/>
 
                     <!-- show toggle button -->
                     <div class="text-center my-3" v-show="showToggleCompletedBtn">
                         <button class="btn btn-sm btn-secondary" 
-                            @click="$event => showCompletedTasks = !showCompletedTasks">
-                            <span v-if="!showCompletedTasks">Show Completed</span>
-                            <span v-else>Hide Completed</span>
+                            @click="showCompletedTasks = !showCompletedTasks">
+                            <span v-if="!showCompletedTasks">Show completed</span>
+                            <span v-else>Hide completed</span>
                         </button>
                     </div>
 
                     <!-- list of completed tasks -->
-                    <Tasks :tasks="completedTasks" :show="completedTaskIsVisible && showCompletedTasks" />
+                    <Tasks :tasks="completedTasks" :show="completedTasksIsVisible && showCompletedTasks"/>
                 </div>
             </div>
         </div>
     </main>
 </template>
-
 <script setup>
-    import { computed, onMounted, ref } from 'vue';
-    import {storeToRefs} from "pinia"
-    import {useTaskStore} from "../stores/task";
+    import { computed, onMounted, ref } from "vue";
+    import { storeToRefs } from "pinia";
+    import { useTaskStore } from "../stores/task";
     import Tasks from "../components/tasks/Tasks.vue";
     import NewTask from "../components/tasks/NewTask.vue";
 
-    const store = useTaskStore();
-    const {completedTasks, uncompletedTasks} = storeToRefs(store);
+    const store = useTaskStore()
+    const { completedTasks, uncompletedTasks } = storeToRefs(store)
     const { fetchAllTasks } = store
 
-    onMounted( async () => {
+    onMounted(async () => {
         await fetchAllTasks()
-    });
+    })
 
     const showToggleCompletedBtn = computed(
         () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
-    );
-    const completedTaskIsVisible = computed(() => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0);
-    const showCompletedTasks = ref(false);
-
-    
-
-    
+    )
+    const completedTasksIsVisible = computed(
+        () => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0
+    )
+    const showCompletedTasks = ref(false || completedTasksIsVisible.value)
 </script>
