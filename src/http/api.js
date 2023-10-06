e39a524626eb15bac491630af0c272f8280ec936
paalaman45 <kissel@sitesatscale.com>
2023-10-06 12:03:05 +0800

    Test

--- src/http/api.js
+++ src/http/api.js
@@ -2,7 +2,11 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    // Default
+    // baseURL: import.meta.env.VITE_BASE_URL,
+
+    //For Netlify Only
+    baseURL: 'http://laravel-api-10052023.atwebpages.com',
 })
 
 export default api
