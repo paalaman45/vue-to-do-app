6c12de6d077193ea323cbd14176efc72e9c3069e
Kissel James Paalaman <jamesvillamilking123@gmail.com>
2023-10-11 19:11:58 +0800

    reset to default

--- src/http/api.js
+++ src/http/api.js
@@ -3,10 +3,7 @@ axios.defaults.withCredentials = true;
 
 const api = axios.create({
     // Default
-    // baseURL: import.meta.env.VITE_BASE_URL,
-
-    //For Netlify Only
-    baseURL: 'http://laravel-api-10052023.atwebpages.com',
+    baseURL: import.meta.env.VITE_BASE_URL,
 })
 
 export default api
